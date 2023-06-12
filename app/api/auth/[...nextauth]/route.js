acb10f5dc4f33fb91e94835ef99eaeb3f6bf6865
Saman <samangithoseinpour@gmail.com>
2023-06-12 13:35:22 +0330

    fix: connection to mongodb

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -12,37 +12,39 @@ const handler = NextAuth({
     }),
   ],
 
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
-
-    session.user.id = sessionUser._id.toString();
-
-    return session;
-  },
-
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
-      // check if a user already exist.
-      const userExist = await User.findOne({
-        email: profile.email,
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
       });
-      // if not, create a new user.
-      if (!userExist) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(' ', '').toLowerCase(),
-          image: profile.picture,
-        });
-      }
 
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+      session.user.id = sessionUser._id.toString();
+
+      return session;
+    },
+
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+        // check if a user already exist.
+        const userExist = await User.findOne({
+          email: profile.email,
+        });
+        // if not, create a new user.
+        if (!userExist) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(' ', '').toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
   },
 });
 
