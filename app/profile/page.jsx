92afb5e4471bbde2cba6918e249ed31dec18772e
Saman <samangithoseinpour@gmail.com>
2023-06-13 13:34:08 +0330

    feat: delete content

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -25,7 +25,27 @@ const MyProfile = () => {
     router.push(`/update-content?id=${post._id}`);
   };
 
-  const handleDelete = async (post) => {};
+  const handleDelete = async (post) => {
+    const confirmedDelete = confirm(
+      'Are you sure you want to delete this content ?'
+    );
+
+    if (confirmedDelete) {
+      try {
+        await fetch(`/api/content/${post._id.toString()}`, {
+          method: 'DELETE',
+        });
+
+        const filteredPosts = posts.filter((p) => {
+          p._id !== post._id;
+        });
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <Profile
